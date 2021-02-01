7010f78aa1485bd7482acf93c7016fa226c25080
onigra <3280467rec@gmail.com>
2021-02-01 16:45:01 +0900

    契約クラスを実装

--- ruby/lib/contract.rb
+++ ruby/lib/contract.rb
@@ -1,2 +1,11 @@
 class Contract
+  attr_reader :product, :signed_on, :revenue, :revenue_recognition, :revenued_at
+
+  def initialize(product:, signed_on:, revenue:, revenue_recognition:, revenued_at:)
+    @product = product
+    @signed_on = signed_on
+    @revenue = revenue
+    @revenue_recognition = revenue_recognition
+    @revenued_at = revenued_at
+  end
 end
--- ruby/test/contract_test.rb
+++ ruby/test/contract_test.rb
@@ -1,2 +1,34 @@
 require 'test/unit'
 require 'contract'
+
+class TC_Contract < Test::Unit::TestCase
+  def test_商品と契約日と売上と売上認識と売上認識日を持って契約が作成できる
+    # given
+    name     = "MS Word"
+    price    = 18800
+    category = "word_processor"
+    product = Product.new(name: name, price: price, category: category)
+
+    # and
+    signed_on = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+    revenue = 18800
+    revenue_recognition = 18800
+    revenued_at = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+
+    # when
+    contract = Contract.new(
+      product: product,
+      signed_on: signed_on,
+      revenue: revenue,
+      revenue_recognition: revenue_recognition,
+      revenued_at: revenued_at,
+    )
+
+    # then
+    assert_equal(contract.product, product)
+    assert_equal(contract.signed_on, signed_on)
+    assert_equal(contract.revenue, revenue)
+    assert_equal(contract.revenue_recognition, revenue_recognition)
+    assert_equal(contract.revenued_at, revenued_at)
+  end
+end
