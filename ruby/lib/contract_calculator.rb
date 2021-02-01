0cef1e8fa854f318c9fa6e0c40d437e2521d6380
onigra <3280467rec@gmail.com>
2021-02-01 16:23:25 +0900

    契約計算クラスを実装

---
+++ ruby/lib/contract_calculator.rb
@@ -0,0 +1,19 @@
+class ContractCalculator
+  def self.calc(price:, category:, signed_on:)
+    if category == "word_processor"
+      { revenue_recognition: price, revenued_at: signed_on}
+    else
+      # カテゴリがspreadsheetの場合、価格の3分の2が売上認識される
+      # 価格が3で割り切れない場合、なるべく早いタイミングで大きい金額で集積確認したいため
+      # 収益認識に端数をプラスする
+      tmp_revenue = price / 3 * 2
+      fraction = price % 3
+      revenue_recognition = tmp_revenue + fraction
+
+      # spreadsheetは契約日から30日後が売上認識日となる
+      revenued_at = signed_on + (86400 * 30)
+      
+      { revenue_recognition: revenue_recognition, revenued_at: revenued_at}
+    end
+  end
+end
---
+++ ruby/test/contract_calculator_test.rb
@@ -0,0 +1,85 @@
+require 'test/unit'
+require 'contract_calculator'
+
+class TC_ContractCalculator < Test::Unit::TestCase
+  sub_test_case "カテゴリがword_processorの場合" do
+    def test_全額売上認識される
+      # given
+      price = 18800
+      category = "word_processor"
+      signed_on = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+
+      # when
+      actual = ContractCalculator.calc(price: price, category: category, signed_on: signed_on)
+
+      # then
+      assert_equal(price, actual[:revenue_recognition])
+    end
+
+    def test_契約日が売上認識日となる
+      # given
+      price = 20000
+      category = "word_processor"
+      signed_on = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+
+      # when
+      actual = ContractCalculator.calc(price: price, category: category, signed_on: signed_on)
+
+      # then
+      assert_equal(signed_on, actual[:revenued_at])
+    end
+  end
+
+  sub_test_case "カテゴリがspreadsheetの場合" do
+    def test_価格の3分の2が売上認識される
+      # given
+      price = 3
+      category = "spreadsheet"
+      signed_on = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+
+      # and
+      expected = 2
+
+      # when
+      actual = ContractCalculator.calc(price: price, category: category, signed_on: signed_on)
+
+      # then
+      assert_equal(expected, actual[:revenue_recognition])
+    end
+
+    def test_契約日から30日後が売上認識日となる
+      # given
+      price = 27800
+      category = "spreadsheet"
+      signed_on = Time.new(2021, 2, 1, 0, 0, 0, "+09:00")
+
+      # and
+      expected = Time.new(2021, 3, 3, 0, 0, 0, "+09:00")
+
+      # when
+      actual = ContractCalculator.calc(price: price, category: category, signed_on: signed_on)
+
+      # then
+      assert_equal(expected, actual[:revenued_at])
+    end
+
+    sub_test_case "価格が3で割り切れない場合" do
+      data("MS Excel", [27800, 18534])
+      data("三四郎",    [5000, 3334])
+      def test_なるべく早いタイミングで大きい金額で集積確認したいため_収益認識に端数をプラスする(data)
+        # given
+        price, expected = data
+
+        # and
+        category = "spreadsheet"
+        signed_on = Time.new(2021, 1, 1, 0, 0, 0, "+09:00")
+
+        # when
+        actual = ContractCalculator.calc(price: price, category: category, signed_on: signed_on)
+
+        # then
+        assert_equal(expected, actual[:revenue_recognition])
+      end
+    end
+  end
+end
